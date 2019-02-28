13d5914f8434cc6d068e95ec5fefc76324ab5bb5
Shaif821 <shaif.bhaggoe@gmail.com>
2019-02-28 14:04:04 +0100

    Added a block component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Block/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Block
   }
 }
 </script>
